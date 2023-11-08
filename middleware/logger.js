e38fdf1753864b05d96b245d7af4e195eb03587a
Dillon Wall <DillonMWall@msn.com>
2023-11-08 19:03:43 +0800

    Lesson 4

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,18 +1,18 @@
-const { format } = require('date-fns')
-const { v4: uuid } = require('uuid')
-const fs = require('fs')
-const fsPromises = require('fs').promises
-const path = require('path')
+const { format } = require("date-fns")
+const { v4: uuid } = require("uuid")
+const fs = require("fs")
+const fsPromises = require("fs").promises
+const path = require("path")
 
 const logEvents = async (message, logFileName) => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
+    const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
 
     try {
-        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+            await fsPromises.mkdir(path.join(__dirname, "..", "logs"))
         }
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+        await fsPromises.appendFile(path.join(__dirname, "..", "logs", logFileName), logItem)
     } catch (err) {
         console.log(err)
     }
@@ -20,7 +20,7 @@ const logEvents = async (message, logFileName) => {
 
 const logger = (req, res, next) => {
     // PROD: This will get full very fast in a production environment, may want to filter these logs somehow
-    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log")
     console.log(`${req.method} ${req.path}`)
     next()
 }
